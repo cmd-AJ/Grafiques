65072138d7b694ebe422fcf97425fb4a5bb9ef9f
Andre <andrewlol774@gmail.com>
2024-07-16 16:50:59 -0600

    Finished all 5 polygons

--- Lab01/src/main.rs
+++ Lab01/src/main.rs
@@ -54,5 +54,45 @@ fn main() {
     fb.set_foreground_color(0xFFFFFF);
     fb.draw_polygon(&verticespol3);
 
+
+    let verticespol4 = vec![
+        Vec3::new(413.0, 177.0, 0.0),
+        Vec3::new(448.0, 159.0, 0.0),
+        Vec3::new(502.0, 88.0, 0.0),
+        Vec3::new(553.0, 53.0, 0.0),
+        Vec3::new(535.0, 36.0, 0.0),
+        Vec3::new(676.0, 37.0, 0.0),
+        Vec3::new(660.0, 52.0, 0.0),
+        Vec3::new(750.0, 145.0, 0.0),
+        Vec3::new(761.0, 179.0, 0.0),
+        Vec3::new(672.0, 192.0, 0.0),
+        Vec3::new(659.0, 214.0, 0.0),
+        Vec3::new(615.0, 214.0, 0.0),
+        Vec3::new(632.0, 230.0, 0.0),
+        Vec3::new(580.0, 230.0, 0.0),
+        Vec3::new(597.0, 215.0, 0.0),
+        Vec3::new(552.0, 214.0, 0.0),
+        Vec3::new(517.0, 144.0, 0.0),
+        Vec3::new(466.0, 180.0, 0.0),
+    ];
+
+    fb.set_foreground_color(0x00FF00);
+    fb.fill_polygon(&verticespol4);
+    fb.set_foreground_color(0xFFFFFF);
+    fb.draw_polygon(&verticespol4);
+
+
+    let poli5 = vec![
+        Vec3::new(682.0, 175.0, 0.0),
+        Vec3::new(708.0, 120.0, 0.0),
+        Vec3::new(735.0, 148.0, 0.0),
+        Vec3::new(739.0, 170.0, 0.0),
+    ];
+
+    fb.set_foreground_color(0xccc2ff);
+    fb.fill_polygon(&poli5);
+    fb.set_foreground_color(0xFFFFFF);
+    fb.draw_polygon(&poli5);
+
     fb.render_buffer("filled_polygon.bmp").unwrap();
 }
